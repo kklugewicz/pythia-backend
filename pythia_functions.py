d60cc2f2a925781c4a984ca79a7a34bcb286f06a
kklugewicz <161895041+kklugewicz@users.noreply.github.com>
2024-03-19 17:08:10 -0400

    Add files via upload

---
+++ pythia_functions.py
@@ -0,0 +1,136 @@
+import yfinance as yf
+import pandas as pd
+import re
+import json
+from flask import request, Flask
+from flask_cors import CORS
+import math
+
+def statement_create(ticker_data,statement_type):
+    if statement_type=="income_statement":
+        return ticker_data.income_stmt
+    elif statement_type=="cashflow":
+        return ticker_data.cashflow
+    else:
+        return ticker_data.balance_sheet
+
+def select_list(statement_type):
+    if statement_type=="income_statement":
+        return ('SGA%', 'R&D%', 'Depreciation %', 'Operating Expense %', 'Interest Expense %','Operating Margin','Total Revenue', 'Cost Of Revenue', 'Gross Profit', 'Gross Profit Margin', 'Pretax Income', 'Net Earnings', 'Basic EPS', 'Net Earnings to Total')
+    if statement_type=="cashflow":
+        return ('Free Cash Flow','Net Income','Net Income From Continuing Operations','Capital Expenditures %', 'Net Common Stock Issuance')
+    if statement_type=="assets_balance_sheet":
+        return ('Cash And Cash Equivalents','Inventory', 'Receivables','Current Assets','Current Ratio','Fixed Asset Turnover Ratio','Total Non Current Assets','Total Assets','Return on Asset Ratio')
+    if statement_type=="liabilities_balance_sheet":
+        return ('Payables And Accrued Expenses','Current Debt', 'Long Term Debt','Current Liabilities','Total Non Current Liabilities Net Minority Interest','Total Liabilities Net Minority Interest','Net Debt','Total Debt','Debt to Shareholders Equity Ratio')
+    if statement_type=="treasury_balance_sheet":
+        return ('Common Stock','Retained Earnings','Treasury Shares Number','Stockholders Equity','Return on Shareholders Equity','Adjusted Return on Equity')
+     
+def add_categories(ticker_data,dict,statement_type,year):
+    output_dict = dict.copy()
+    financials=ticker_data.financials
+    column_dates = pd.to_datetime(financials.columns)
+    for date in column_dates:
+        if date.year == year:
+            use_year = date
+    use_year = str(use_year.strftime('%Y-%m-%d'))
+    output_dict['Net Income']=financials.loc['Net Income',use_year]
+    if statement_type=="income_statement":
+        output_dict['Net Earnings'] = (output_dict.get('Operating Revenue', 0) - 
+                                    output_dict.get('Cost of Revenue', 0) - 
+                                    (output_dict.get('Operating Expense', 0) + 
+                                        output_dict.get('Research And Development', 0) + 
+                                        output_dict.get('Selling General And Administration', 0)) - 
+                                    output_dict.get('Interest Expense', 0) - 
+                                    output_dict.get('Tax Provision', 0) - 
+                                    output_dict.get('Other Non Operating Income Expenses', 0) + 
+                                    output_dict.get('Interest Income Non Operating', 0))
+        output_dict['Operating Margin']=(output_dict.get('Operating Income')/output_dict.get('Total Revenue'))*100
+        output_dict['Gross Profit Margin'] = output_dict.get('Gross Profit', 0) / output_dict.get('Total Revenue', 1)
+        output_dict['SGA%'] = output_dict.get('Selling General And Administration', 0) / output_dict.get('Gross Profit', 1)
+        output_dict['R&D%'] = output_dict.get('Research And Development', 0) / output_dict.get('Gross Profit', 1)
+        output_dict['Depreciation %'] = output_dict.get('Reconciled Depreciation', 0) / output_dict.get('Gross Profit', 1)
+        output_dict['Operating Expense %'] = output_dict.get('Operating Expense', 0) / output_dict.get('Gross Profit', 1)
+        output_dict['Net Earnings to Total'] = output_dict.get('Net Earnings', 0) / output_dict.get('Total Revenue', 1)
+        output_dict['Interest Expense %'] = output_dict.get('Interest Expense', 0) / output_dict.get('Total Revenue', 1)
+        return output_dict
+    elif statement_type=="cashflow":
+        output_dict['Capital Expenditures %'] = output_dict.get('Capital Expenditure', 0) / output_dict['Net Income']
+        return output_dict
+    else:
+        output_dict['Fixed Asset Turnover Ratio']=financials.loc['Total Revenue',use_year]/output_dict.get('Net PPE')
+        output_dict['Current Ratio'] = output_dict.get('Current Assets', 0) / output_dict['Current Liabilities']
+        output_dict['Return on Asset Ratio']= output_dict.get('Net Income', 0) / output_dict['Total Assets']
+        output_dict['Debt to Shareholders Equity Ratio'] = output_dict.get('Total Liabilities Net Minority Interest', 0) / output_dict['Stockholders Equity']
+        output_dict['Treasury-adjusted Debt Shareholders Equity Ratio']= output_dict.get('Total Liabilities Net Minority Interest', 0) / (output_dict['Stockholders Equity'] + output_dict['Capital Stock'])
+        output_dict['Return on Shareholders Equity'] = output_dict.get('Net Income', 0) / output_dict['Stockholders Equity']
+        return output_dict
+
+def categories_list(statement):
+    row_names = statement.index.tolist()
+    return row_names
+
+def years_list(statement):
+    years = []
+    pattern=r'\b\d{4}\b'
+    num_rows=statement.shape[1]
+    for i in range(num_rows):
+        Year=str(statement.columns[i])
+        Year=re.findall(pattern,Year)
+        years.append(int(Year[0]))
+    return years
+
+def DictMake(statement,val):
+    dict={}
+    for index, row in statement.iterrows():
+        dict[index] = row[statement.columns[val]]
+    return dict
+
+def process_data(data):
+    processed_data = {}
+    for year, values in data.items():
+        processed_values = {}
+        for key, value in values.items():
+            if isinstance(value, float) and math.isnan(value):
+                processed_values[key] = 0
+            else:
+                processed_values[key] = value
+        processed_data[year] = processed_values
+    return processed_data
+
+def yoy(dict,years,select_categories):
+    yoy_dict={}
+    thisyear=years[0]
+    thisyear_dict=dict[thisyear]
+    lastyear=years[1]
+    lastyear_dict=dict[lastyear]
+    for element in select_categories:
+        if lastyear_dict[element]==0:
+            yoy_dict[element]=0
+        else:
+            yoy_dict[element]=(thisyear_dict[element]-lastyear_dict[element])/lastyear_dict[element]
+    return yoy_dict
+
+def keys_to_strings(dict):
+    return {str(key): value for key, value in dict.items()}
+
+
+def final_dict(ticker_data,statement_type):
+    statement=statement_create(ticker_data,statement_type)
+    select_categories=select_list(statement_type)
+    all_categories=categories_list(statement)
+    years=years_list(statement)
+    complete_dict={}
+    val=0
+    for year in years:
+        basic_dict=DictMake(statement,val)
+        full_dict=add_categories(ticker_data,basic_dict,statement_type,year)
+        for element in all_categories:
+            if element not in select_categories:
+                del full_dict[element]
+        complete_dict[year]=full_dict
+        val=val+1
+    complete_dict["Year over year(past year)"]=yoy(complete_dict,years,full_dict.keys())
+    processed_data=process_data(complete_dict)
+    corrected_dict=keys_to_strings(processed_data)
+    return corrected_dict
---
+++ valuation.py
@@ -0,0 +1,108 @@
+import yfinance as yf
+import pandas as pd
+import re
+import json
+from flask import request, Flask
+from flask_cors import CORS
+import math
+from pythia_functions import *
+
+def discounted_cashflow(table, ticker_data):
+    discount_rate = 0.095
+    perpetual_growth_rate = 0.025
+    yearly_free_cash_flows = []
+    data = table["CF"]
+    keys = list(data.keys()) 
+    keys.remove("Year over year(past year)")  
+    company_growth_rate = []
+
+    for key in keys:
+        year = data[key]
+        free_cash_flow = year["Free Cash Flow"]
+        yearly_free_cash_flows.append(free_cash_flow)
+
+    print(yearly_free_cash_flows)
+
+    for i in range(len(yearly_free_cash_flows) - 1):
+        this_year = yearly_free_cash_flows[i]
+        last_year = yearly_free_cash_flows[i + 1]
+        growth_rate = (this_year - last_year) / last_year
+        company_growth_rate.append(growth_rate)
+
+    print(company_growth_rate)
+
+    average_growth_rate = sum(company_growth_rate) / len(company_growth_rate)
+    print(average_growth_rate)
+    FFCF = []
+    this_year_FCF = yearly_free_cash_flows[0]
+
+    for i in range(9):  
+        year_FFCF = this_year_FCF + (average_growth_rate * this_year_FCF)
+        FFCF.append(year_FFCF)
+        this_year_FCF = year_FFCF
+
+    FFCF_terminal_value = (FFCF[8] * (1 + perpetual_growth_rate)) / (perpetual_growth_rate + discount_rate)
+    print("FFCF8")
+    print (FFCF[8])
+    FFCF.append(FFCF_terminal_value)
+
+    print("FFCF")
+    print(FFCF)
+
+    PV_FFCF = []
+    for i in range(9):  
+        PV = FFCF[i] / ((1 + discount_rate) ** (i + 1))
+        PV_FFCF.append(PV)
+
+    print(PV_FFCF)
+    
+    sum_PV_FFCF = sum(PV_FFCF)
+
+    balance_sheet = ticker_data.balance_sheet
+    balance_sheet = DictMake(balance_sheet, 0)  
+    cash_and_equivalents = balance_sheet['Cash And Cash Equivalents']
+    total_debt = balance_sheet['Total Debt']
+    total_shares_outstanding = ticker_data.info.get('sharesOutstanding')
+    equity_value = sum_PV_FFCF + cash_and_equivalents - total_debt
+    dcf = equity_value / total_shares_outstanding
+    print (dcf)
+    return dcf
+    
+def growth_rate(table):
+    data = table["IS"]
+    keys = list(data.keys()) 
+    earnings=[]
+    keys.remove("Year over year(past year)")  
+    company_growth_rate = []
+
+    for key in keys:
+        year = data[key]
+        year_earnings = year["Net Earnings"]
+        earnings.append(year_earnings)
+
+    for i in range(len(earnings) - 1):
+        this_year = earnings[i]
+        last_year = earnings[i + 1]
+        growth_rate = (this_year - last_year) / last_year
+        company_growth_rate.append(growth_rate)
+
+
+    average_growth_rate = sum(company_growth_rate) / len(company_growth_rate)
+    return average_growth_rate
+
+def valuation(table,ticker_data):
+    valuation_table={}
+    valuation_table["Current Stock Price"]=ticker_data.info.get('currentPrice')
+    valuation_table["Trailing P/E"]=ticker_data.info.get('trailingPE')
+    valuation_table["Forward P/E"]=ticker_data.info.get('forwardPE')
+    valuation_table["Trailing PEG Ratio"]=ticker_data.info.get('trailingPegRatio')
+    valuation_table["P/FCF"]=ticker_data.info.get('freeCashflow')/ticker_data.info.get('currentPrice')
+    #valuation_table["Discounted CashFlow"]=discounted_cashflow(table,ticker_data)
+    earnings_growth_rate=growth_rate(table)
+    #valuation_table["Peter Lynch's Valuation"]=(earnings_growth_rate+ticker_data.info.get('dividendYield'))/ticker_data.info.get('trailingPE')
+    print (valuation_table)
+    current_valuation_table={}
+    current_valuation_table["current"]=valuation_table
+    processed_data=process_data(current_valuation_table)
+    corrected_dict=keys_to_strings(processed_data)
+    return corrected_dict
